4b4897e537eaeb865f94ec4252d3b35e97ba4106
Exequiel Sosa <exequielsosa@gmail.com>
2023-05-06 22:01:43 -0300

    add wp

---
+++ components/floatingWP.js
@@ -0,0 +1,14 @@
+const FloatingWhatsApp = () => {
+  return (
+    <a
+      href="https://wa.me/541158959823"
+      className="whatsapp_float"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i className="fa fa-whatsapp whatsapp-icon"></i>
+    </a>
+  );
+};
+
+export default FloatingWhatsApp;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
+import "@/styles/whatsApp.css";
 import Layout from "@/components/layout";
+import FloatingWhatsApp from "@/components/floatingWP";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <FloatingWhatsApp />
       <Component {...pageProps} />
     </Layout>
   );
