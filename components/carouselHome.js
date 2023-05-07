8a2688cecd19d39726ae15126c6408f23c962f1c
Exequiel Sosa <exequielsosa@gmail.com>
2023-05-06 21:52:58 -0300

    add text

--- components/carouselHome.js
+++ components/carouselHome.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const CarouselHome = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slide={false}>
+      <Carousel slideInterval={5000}>
         <Link href="/impuestos">
           <img src="/impuestos.jpg" alt="Liquidación de Impuestos" />
           <span class="absolute top-1/2 left-1/2 text-2xl font-bold text-fuchsia-600 -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800 bg-indigo-200 opacity-75 font-['Titillium_Web'] tracking-wider">
@@ -13,13 +13,13 @@ const CarouselHome = () => {
           </span>
         </Link>
         <Link href="/sueldos">
-          <img src="sueldos.webp" alt="Liquidacion de Sueldos" />
+          <img src="sueldos.jpg" alt="Liquidacion de Sueldos" />
           <span class="absolute top-1/2 left-1/2 text-2xl font-bold text-fuchsia-600 -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800 bg-indigo-200 opacity-75 font-['Titillium_Web'] tracking-wider">
             LIQUIDACION DE HABERES
           </span>
         </Link>
         <Link href="/tramites">
-          <img src="tramites.webp" alt="Tramites" />
+          <img src="tramites.jpg" alt="Tramites" />
           <span class="absolute top-1/2 left-1/2 text-2xl font-bold text-fuchsia-600 -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800 bg-indigo-200 opacity-75 font-['Titillium_Web'] tracking-wider">
             GESTION DE TRAMITES
           </span>
--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,30 @@ const Home = () => {
       </Head>
       <main>
         <CarouselHome />
+        
+        <section>
+          <div className="flex w-full justify-center flex-col items-center">
+          <div className="flex justify-center flex-col w-11/12">
+          <article>
+            <h2 className="mt-6 text-xl font-semibold">Estudio Contable Dillon</h2>
+            <br />
+            <p>
+              En Estudio Contable Dillon entendemos que preferís dedicarle la
+              menor cantidad de tiempo a tus obligaciones impositivas, para
+              poder concentrarte en ganar nuevos negocios y disfrutar de tu
+              tiempo libre. Por eso nuestro principal objetivo es darte la
+              tranquilidad de que tus impuestos están en orden. <br/>Trabajamos con
+              cientos de monotributistas, startups y empresas de todas las
+              formas y tamaños de todo el país. Nos hemos esforzado mucho en
+              escuchar sus necesidades, invertir en tecnología y capacitar a
+              nuestros contadores para brindarles un alto nivel de servicio.
+            </p>
+          </article>
+          </div>
+          </div>
+        </section>
+        
+      
       </main>
     </>
   );
