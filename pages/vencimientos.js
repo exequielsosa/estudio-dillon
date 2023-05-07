a5497a2477ac1c2260a3bc0fd0c7c9e7f4d19ffc
Exequiel Sosa <exequielsosa@gmail.com>
2023-05-07 12:41:23 -0300

    start vencimientos

--- pages/vencimientos.js
+++ pages/vencimientos.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import HeaderImage from "@/components/headerImage";
+import ModalImage from "react-modal-image";
 
 const Vencimientos = () => {
   return (
@@ -10,17 +12,30 @@ const Vencimientos = () => {
         <meta name="robots" content="index,follow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <p>Vencimientos</p>
+      <main>        
+        <div className="flex w-full justify-center flex-col items-center">
+          <div className="flex justify-center flex-col w-11/12">
+            <article>
+              <h1 className="mt-6 mb-6 text-xl font-semibold">
+                Calendario de vencimientos impositivos para el 2023
+              </h1>
+
+              <div className="flex w-full justify-center flex-col items-center mt-6">
+
+              <ModalImage
+                small="/calendarioVencimientos2023SmallR.png"
+                large="/calendarioVencimientos2023.png"
+                alt="Hello World!"
+              />
+              </div>
+              <br />
+              <div align="center" className="vencimientos"></div>
+            </article>
+          </div>
+        </div>
       </main>
     </>
   );
 };
-
-// export async function getServerSideProps() {
-//   return {
-//     props: {},
-//   };
-// }
 
 export default Vencimientos;
