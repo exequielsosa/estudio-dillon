f41424e1ff97de782ea351d3f7db9297d49fb334
Exequiel Sosa <exequielsosa@gmail.com>
2023-08-05 10:02:18 -0300

    add analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -2,32 +2,48 @@ import "@/styles/globals.css";
 import "@/styles/whatsApp.css";
 import Layout from "@/components/layout";
 import FloatingWhatsApp from "@/components/floatingWP";
-import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script'
+import { Analytics } from "@vercel/analytics/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../gatg";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
-     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-VS1FLWWXPP"/>
-    <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-VS1FLWWXPP', {
-            page_path: window.location.pathname,
-          });
-        `,
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
-    />
-    <Layout>
-      <FloatingWhatsApp />
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+      />
+      <Layout>
+        <FloatingWhatsApp />
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
     </>
   );
 }
